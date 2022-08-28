bee5883db088394ff890476e6e3077b7ddda16bd
Wilber Paredes <wilberparedes@gmail.com>
2022-08-28 00:09:00 -0500

    feat: create service for manage data in database

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
-
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './interface/product.interface';
+import { CreateProductDTO } from './dto/product.dto';
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>,
+  ) {}
+
+  async getProducts(): Promise<Product[]> {
+    return await this.productModel.find();
+  }
+
+  async getProduct(productID: string): Promise<Product> {
+    return await this.productModel.findById(productID);
+  }
+
+  async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
+    const product = await new this.productModel(createProductDTO).save();
+    return product;
+  }
+
+  async deleteProduct(productID: string): Promise<Product> {
+    return await this.productModel.findByIdAndDelete(productID);
+  }
+
+  async updateProduct(
+    productID: string,
+    createProductDTO: CreateProductDTO,
+  ): Promise<Product> {
+    return await this.productModel.findByIdAndUpdate(
+      productID,
+      createProductDTO,
+      { new: true },
+    );
+  }
+}
